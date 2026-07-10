3d0e5aaff6ba37ba7131c99a0e1197aa093cda7f
Daril <u202317338@upc.edu.pe>
2026-07-09 23:40:02 -0500

    Add navigation functionality and new login/register pages.

---
+++ docs/assets/scripts/despliegue.js
@@ -0,0 +1,17 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById('hamburger');
+    const navContainer = document.getElementById('nav-container');
+
+    // Comprobamos si JavaScript encuentra los elementos
+    console.log("Botón hamburguesa:", hamburger);
+    console.log("Contenedor del menú:", navContainer);
+
+    if (hamburger && navContainer) {
+        hamburger.addEventListener('click', () => {
+            navContainer.classList.toggle('active');
+            console.log("Se hizo clic, clase 'active' modificada.");
+        });
+    } else {
+        console.error("¡Error! No se encontró el botón o el menú en el HTML.");
+    }
+});
