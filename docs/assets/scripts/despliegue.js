0d2769cbeff8e7bf3002cd62d25bd1072433a6cd
Daril <u202317338@upc.edu.pe>
2026-07-10 00:01:46 -0500

    Remove console logs and error handling from despliegue.js

--- docs/assets/scripts/despliegue.js
+++ docs/assets/scripts/despliegue.js
@@ -2,15 +2,9 @@
     const hamburger = document.getElementById('hamburger');
     const navContainer = document.getElementById('nav-container');
 
-    console.log("Botón hamburguesa:", hamburger);
-    console.log("Contenedor del menú:", navContainer);
-
     if (hamburger && navContainer) {
         hamburger.addEventListener('click', () => {
             navContainer.classList.toggle('active');
-            console.log("Se hizo clic, clase 'active' modificada.");
         });
-    } else {
-        console.error("¡Error! No se encontró el botón o el menú en el HTML.");
     }
 });
